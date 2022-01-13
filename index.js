9741cd265dca06ef7defa57a1b619595991004cf
Amar Katechia <amar.katechia@gmail.com>
2022-01-12 20:43:27 -0600

    added debounce input util

---
+++ index.js
@@ -0,0 +1,17 @@
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '9b64c39a',
+      s: searchTerm
+    }
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+const onInput = event => {
+  fetchData(event.target.value);
+};
+input.addEventListener('input', debounce(onInput, 500));
